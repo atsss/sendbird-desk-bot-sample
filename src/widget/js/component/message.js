5f79823b7438ff634145164b16641c8e8ff93db4
atsss <atsushi0623itoh@gmail.com>
2025-01-10 21:14:36 +0000

    Customize regular expression to detect URL

--- src/widget/js/component/message.js
+++ src/widget/js/component/message.js
@@ -5,6 +5,7 @@ import { simplify } from '../simplify.js';
 import { parseDom } from '../domparser.js';
 import { getSb } from '../globalStore.js';
 
+
 export default class MessageElement {
   constructor(message, streak) {
     this.message = message;
@@ -225,6 +226,7 @@ export default class MessageElement {
         }
       }
 
+      const CUSTOM_REGULAR_EXPRESSION = /(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#][a-zA-Z0-9/?.&=_-]*)?/gi
       /// message
       if (this.message.isAdminMessage()) {
         this.messageFile.hide();
@@ -250,9 +252,11 @@ export default class MessageElement {
             window.open(this.message.url, '_blank');
           });
         }
-      } else if (SendBirdDesk.Message.UrlRegExp.test(this.message.message)) {
+      // } else if (SendBirdDesk.Message.UrlRegExp.test(this.message.message)) {
+      } else if (CUSTOM_REGULAR_EXPRESSION.test(this.message.message)) {
         this.message.message = this.message.message.replace(
-          SendBirdDesk.Message.UrlRegExp,
+          // SendBirdDesk.Message.UrlRegExp,
+          CUSTOM_REGULAR_EXPRESSION,
           url => `<a href='${url}'>${url}</a>`
         );
 
